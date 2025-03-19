08d865f51714b05741184be34a4c5037ab929f49
itsmeriski <riskifaozi28.me@gmail.com>
2025-03-20 06:48:26 +0700

    update belajar slicing web

---
+++ app.js
@@ -0,0 +1,29 @@
+const contentLearningCards = [
+    {
+        image: "images/Mask Group.png",
+        altImage: "",
+        title: "Starter",
+        desc: "Free class access for that start learning code and design",
+    },
+    {
+        image: "images/Mask Group-1.png",
+        altImage: "",
+        title: "Premium",
+        desc: "Advanced material from the Starter & learn to build projects",
+    },
+    {
+        image: "images/Mask Group-2.png",
+        altImage: "",
+        title: "Superstar",
+        desc: "Materials for experts who have completed the premium package",
+    },
+    {
+        image: "images/Mask Group-3.png",
+        altImage: "",
+        title: "Bootcamp",
+        desc: "Informatics science training program with the latest material",
+    },
+];
+
+const learningCards = document.getElementsByClassName("learning__cards");
+
